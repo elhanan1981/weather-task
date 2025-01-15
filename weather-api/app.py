45ed60f3ec7212606b7eea552b9a432f4a7ab761
elhanan katiei <brachivahav@gmail.com>
2025-01-15 14:13:23 +0200

    refactoring the running system

--- weather-api/app.py
+++ weather-api/app.py
@@ -1,47 +1,18 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI
 from dotenv import load_dotenv
-import requests
-import os
-import urllib.request
-import sys
-import json
-from database_setup import initialize_database
-# from database import get_db, engine, Base
-import database
+from database.create_tables import initialize_database
+
 import logging
 
 load_dotenv()
 app = FastAPI()
-
-# Initialize database on startup
-@app.on_event("startup")
+        
+@app.put("/init")
 async def startup_event():
     if not initialize_database():
         logging.error("Failed to initialize database!")
-
-@app.get("/weather/last30days/{city}")
-async def get_weather(city):
-    print('hi')
-    api_key = "HUZLDMPCUMZDGQ5KVRX3SLYSB"
-    url = f"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Los%20angeles/last30days?unitGroup=metric&key=HUZLDMPCUMZDGQ5KVRX3SLYSB&contentType=json"
-    
-    try: 
-        # ResultBytes = urllib.request.urlopen(url)
-  
-        # data = json.load(ResultBytes)
-
-        return {
-            "city": city,
-            "data": 'data'
-        }
-    except urllib.error.HTTPError  as e:
-        ErrorInfo= e.read().decode() 
-        print('Error code: ', e.code, ErrorInfo)
-        sys.exit()
-    except  urllib.error.URLError as e:
-        ErrorInfo= e.read().decode() 
-        print('Error code: ', e.code,ErrorInfo)
-        sys.exit()
+        return {"the init failed!!"}
+    return {"the init successed!!"}
 
 @app.get("/")
 async def read_root():
